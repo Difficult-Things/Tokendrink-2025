2c158048d9b9ce351841bdc77856b782838e14b3
Joris Lodewijks <github@jorislodewijks.nl>
2025-04-28 19:42:36 +0200

    feat: add gray variants for beer and soda products

--- specifications/products.ts
+++ specifications/products.ts
@@ -3,11 +3,13 @@ export const beerRed = "Red_Beer";
 export const beerGreen = "Green_Beer";
 export const beerOrange = "Orange_Beer";
 export const beerPurple = "Purple_Beer";
+export const beerGray = "Gray_Beer";
 
 export const sodaBlue = "Soda_Blue";
 export const sodaRed = "Soda_Red";
 export const sodaGreen = "Soda_Green";
 export const sodaOrange = "Soda_Orange";
 export const sodaPurple = "Soda_Purple";
+export const sodaGray = "Soda_Gray";
 
 export type ProductType = "beer" | "soda";
